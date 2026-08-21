1fdf4be9b5d0a43371d284f86db4504c55a06e60
Suhas <suhasnarapogu@gmail.com>
2026-08-21 21:05:47 +0530

    Completed Tanh classification for LS and NLS datasets

--- src/main.cpp
+++ src/main.cpp
@@ -107,6 +107,106 @@ void run_nls_classification(){
 }
 
 
+vector<DataPoint> filter_relabel_tanh(const vector<DataPoint>& full_data,double target_class,double negative_class ){
+    vector<DataPoint> filtered;
+
+    for(size_t i = 0; i < full_data.size(); i++){
+        if(full_data[i].label == target_class){
+            DataPoint pt = full_data[i];
+            pt.label = 1.0;
+            filtered.push_back(pt);
+        }else if(full_data[i].label == negative_class){
+            DataPoint pt = full_data[i];
+            pt.label = -1.0;
+            filtered.push_back(pt);
+        }
+    }
+
+    return filtered;
+}
+
+
+double calculate_accuracy_tanh(Perceptron& p,const vector<DataPoint>& test_data){
+    int correct_guesses = 0;
+
+    for(size_t i = 0; i < test_data.size(); i++){
+        double pred = p.predict(test_data[i].features);
+        double guess;
+        if(pred >= 0.0){
+            guess = 1.0;
+        }else{
+            guess = -1.0;
+        }
+        if(guess == test_data[i].label){
+            ++correct_guesses;
+        }
+    }
+
+    return (static_cast<double>(correct_guesses) / test_data.size()) * 100.0;
+}
+
+
+void helper_tanh(const vector<DataPoint>& train,const vector<DataPoint>& test,double target,double negative,const string& match_name){
+    cout << " Match: " << match_name << endl;
+
+    vector<DataPoint> train_subset = filter_relabel_tanh(train,target,negative);
+    vector<DataPoint> test_subset = filter_relabel_tanh(test,target,negative);
+
+    int input_size = train[0].features.size();  
+    Perceptron p(input_size,0.001,getTanh());
+
+    int epochs = 1000;
+
+    for(int e = 0; e < epochs ; e++){
+        for(size_t i = 0 ; i < train_subset.size(); i++){
+            p.train_step(train_subset[i].features,train_subset[i].label);
+        }       
+    }
+
+    double accuracy = calculate_accuracy_tanh(p, test_subset);
+    cout << " Accuracy: " << accuracy << "%" << endl;
+}
+
+
+void run_ls_tanh(){
+    cout << " Linearly Separable(One vs One)" << endl;
+
+    vector<DataPoint> train_set;
+    vector<DataPoint> test_set;
+
+    Dataloader::loadLSData(
+        "Data/Classification/LS_Group26/Class1.txt",
+        "Data/Classification/LS_Group26/Class2.txt",
+        "Data/Classification/LS_Group26/Class3.txt",
+        train_set,
+        test_set
+    );
+
+    helper_tanh(train_set, test_set, 0.0, 1.0, "Class 0 vs Class 1");
+    helper_tanh(train_set, test_set, 0.0, 2.0, "Class 0 vs Class 2");
+    helper_tanh(train_set, test_set, 1.0, 2.0, "Class 1 vs Class 2");
+
+}
+
+
+void run_nls_tanh(){
+    cout << " Non Linearly Separable (One vs One) TanH" << endl;
+
+    vector<DataPoint> train_set;
+    vector<DataPoint> test_set;
+
+    Dataloader::loadNLSData(
+        "Data/Classification/NLS_Group26.txt",
+        train_set,
+        test_set
+    );
+
+    helper(train_set, test_set, 0.0, 1.0, "Class 0 vs Class 1");
+    helper(train_set, test_set, 0.0, 2.0, "Class 0 vs Class 2");
+    helper(train_set, test_set, 1.0, 2.0, "Class 1 vs Class 2");
+}
+
+
 void run_bivariate(){
     cout << " Running Regression (Bivariate) " <<  endl;
 
@@ -203,6 +303,8 @@ int main(){
     // run_bivariate();
     // run_univariate();
     // run_ls_classification();
-    run_nls_classification();
+    // run_nls_classification();
+    // run_ls_tanh();
+    run_nls_tanh();
     return 0;
 }
